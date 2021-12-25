4e081fc1ca05c610badf00664299c20b52cbfd21
ugurunal778 <ugur778@gmail.com>
2021-12-25 22:46:38 +0300

    25.12.2021

--- ECommerceSiteProject.WebUI/Entity/Category.cs
+++ ECommerceSiteProject.WebUI/Entity/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +11,12 @@ namespace ECommerceSiteProject.WebUI.Entity
     {   //kategorinin Id'si
         public int Id { get; set; }
         //kategori içeriği
+        [DisplayName("Kategori Adı")]
+        [StringLength(maximumLength:20, ErrorMessage ="En fazla 20 karakter girilebilir.")]
         public string Name { get; set; }
+        [DisplayName("Açıklama")]
         public string Description { get; set; }
         //tablo ilişkilendirmesi
         public List<Product> Products { get; set; }
-
     }
 }
--- ECommerceSiteProject.WebUI/Entity/Product.cs
+++ ECommerceSiteProject.WebUI/Entity/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,11 +10,16 @@ namespace ECommerceSiteProject.WebUI.Entity
     {   //ürün Id'si
         public int Id { get; set; }
         //Ürün içeriği
+        [DisplayName("Ürün Adı")]
         public string Name { get; set; }
+        [DisplayName("Ürün Açıklaması")]
         public string Description { get; set; }
+        [DisplayName("Ürün Fiyatı")]
         public double Price { get; set; }
+        [DisplayName("Ürün Stoğu")]
         public int Stock{ get; set; }
         public string Image { get; set; }
+        [DisplayName("Anasayfa Ürünü")]
         public bool IsHome { get; set; }
         public bool IsApproved { get; set; }
         //tablo ilişkilendirmesi
