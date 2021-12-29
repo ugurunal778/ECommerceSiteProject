cc14950653204323751be9d440d9c8845d9216f2
ugurunal778 <ugur778@gmail.com>
2021-12-29 22:31:50 +0300

    29.12.2021

---
+++ ECommerceSiteProject.WebUI/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using ECommerceSiteProject.WebUI.Entity;
+using ECommerceSiteProject.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceSiteProject.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private DataContext db = new DataContext();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            var product = db.Products.FirstOrDefault(x => x.Id == id);
+            if (product!=null)
+            {
+                GetCart().AddProduct(product, 1);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromCart(int id)
+        {
+            var product = db.Products.FirstOrDefault(x => x.Id == id);
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            var cart = (Cart)Session["Cart"];
+            if (cart==null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+        public PartialViewResult Summary()
+        {
+            return PartialView(GetCart());
+        }
+    }
+}
